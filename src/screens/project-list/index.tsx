96c61cf78c58abb7c1ac6456a8d26cba71c46627
yangdepp <yangdepp@163.com>
2021-06-19 00:08:11 +0800

    feat: add storgae login

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
-import qs from "qs";
 import { cleanObject, useDebounce, useMount } from "../../utils";
-
-const apiURL = process.env.REACT_APP_API_URL;
+import { useHttp } from "utils/http";
 
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
@@ -15,23 +13,14 @@ export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
 
   const debouncedParam = useDebounce(param, 500);
-
+  const client = useHttp();
   useEffect(() => {
-    fetch(
-      `${apiURL}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
+    // eslint-disable-next-line
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${apiURL}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
 
   return (
