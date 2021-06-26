7456b7a3a5eb5026806192d6f2d949e430c027c1
yangdepp <yangdepp@163.com>
2021-06-26 23:10:21 +0800

    project list loading & error

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,43 +1,32 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import { SearchPanel } from "screens/project-list/search-panel";
-import { List, Project } from "screens/project-list/list";
-import { cleanObject, useDebounce, useMount } from "../../utils";
-import { useHttp } from "utils/http";
+import { List } from "screens/project-list/list";
+import { useDebounce } from "../../utils";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
-import { useAsync } from "utils/use-async";
+import { useProjects } from "utils/project";
+import { useUsers } from "utils/user";
 
 // 使用 JS 的同学，大部分的错误都是在 runtime(运行时) 的时候发现的
 // 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
-// const apiUrl = process.env.REACT_APP_API_URL;
 
 export const ProjectListScreen = () => {
-  const [users, setUsers] = useState([]);
   const [param, setParam] = useState({
     name: "",
     personId: "",
   });
-  const debouncedParam = useDebounce(param, 200);
-  const client = useHttp();
-  const { run, isLoading, error, data: list } = useAsync<Project[]>();
-
-  useEffect(() => {
-    run(client("projects", { data: cleanObject(debouncedParam) }));
-    // eslint-disable-next-line
-  }, [debouncedParam]);
-
-  useMount(() => {
-    client("users").then(setUsers);
-  });
+  const debouncedParam = useDebounce(param, 500);
+  const { isLoading, error, data: list } = useProjects(debouncedParam);
+  const { data: users } = useUsers();
 
   return (
     <Container>
       <h1>项目列表</h1>
-      <SearchPanel users={users} param={param} setParam={setParam} />
+      <SearchPanel users={users || []} param={param} setParam={setParam} />
       {error ? (
         <Typography.Text type="danger">{error.message}</Typography.Text>
       ) : null}
-      <List users={users} dataSource={list || []} loading={isLoading} />
+      <List users={users || []} dataSource={list || []} loading={isLoading} />
     </Container>
   );
 };
---
+++ src/utils/project.ts
@@ -0,0 +1,16 @@
+import { useAsync } from "utils/use-async";
+import { Project } from "screens/project-list/list";
+import { useEffect } from "react";
+import { cleanObject } from "utils";
+import { useHttp } from "./http";
+
+export const useProjects = (param?: Partial<Project>) => {
+  const client = useHttp();
+  const { run, ...result } = useAsync<Project[]>();
+
+  useEffect(() => {
+    run(client("projects", { data: cleanObject(param || {}) }));
+    // eslint-disable-next-line
+  }, [param]);
+  return result;
+};
