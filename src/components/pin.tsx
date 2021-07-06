878254ee38dcb3ef975a08ad5acbca6eee6f180f
yangdepp <yangdepp@163.com>
2021-07-07 00:12:15 +0800

    pin project

---
+++ src/components/pin.tsx
@@ -0,0 +1,18 @@
+import { Rate } from "antd";
+
+interface PinProps extends React.ComponentProps<typeof Rate> {
+  checked: boolean;
+  onCheckedChange?: (checked: boolean) => void;
+}
+
+export const Pin = (props: PinProps) => {
+  const { checked, onCheckedChange, ...restProps } = props;
+  return (
+    <Rate
+      {...restProps}
+      count={1}
+      value={checked ? 1 : 0}
+      onChange={(num) => onCheckedChange?.(!!num)}
+    />
+  );
+};
--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -3,6 +3,8 @@ import { User } from "screens/project-list/search-panel";
 import { Table, TableProps } from "antd";
 import dayjs from "dayjs";
 import { Link } from "react-router-dom";
+import { Pin } from "components/pin";
+import { useEditProject } from "utils/project";
 
 export interface Project {
   id: number;
@@ -18,11 +20,24 @@ interface ListProps extends TableProps<Project> {
 }
 
 export const List = ({ users, ...props }: ListProps) => {
+  const { mutate } = useEditProject();
+  const pinProject = (id: number) => (pin: boolean) => mutate({ id, pin });
   return (
     <Table
       rowKey={"id"}
       pagination={false}
       columns={[
+        {
+          title: <Pin checked={true} disabled={true} />,
+          render(value, project) {
+            return (
+              <Pin
+                checked={project.pin}
+                onCheckedChange={pinProject(project.id)}
+              />
+            );
+          },
+        },
         {
           title: "名称",
           sorter: (a, b) => a.name.localeCompare(b.name),
