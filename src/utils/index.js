0485ac9f2f02cf862a229743eeab6456197f5744
yangdepp <yangdepp@163.com>
2021-10-30 14:28:41 +0800

    feat: 使用自定义hook useMount useDebounce

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const cleanObject = (object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
@@ -10,3 +12,20 @@ export const cleanObject = (object) => {
 };
 
 export const isFalsy = (value) => (value === 0 ? false : !value);
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debounceValue;
+};
