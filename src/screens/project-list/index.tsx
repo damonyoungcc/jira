e82599cfd3e4e9e0cf34d300cb03191996601d7b
yangdepp <yangdepp@163.com>
2021-11-02 23:30:13 +0800

    添加登录状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
-import qs from "qs";
 import { cleanObject, useDebounce, useMount } from "../../utils";
-
-const apiURL = process.env.REACT_APP_API_URL;
+import { useHttp } from "utils/http";
 
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
@@ -14,22 +12,16 @@ export const ProjectListScreen = () => {
   const debouncedParam = useDebounce(param, 200);
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
+
+  const client = useHttp();
+
   useEffect(() => {
-    fetch(
-      `${apiURL}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
+    // eslint-disable-next-line
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${apiURL}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
 
   return (
