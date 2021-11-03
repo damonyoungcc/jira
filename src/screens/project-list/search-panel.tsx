c808fc4b5e220179bd7648f541836533e10b2526
yangdepp <yangdepp@163.com>
2021-11-04 00:01:34 +0800

    feat: 引入antd

--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { Input, Select } from "antd";
+
 export interface User {
   id: string;
   name: string;
@@ -18,24 +21,35 @@ interface SearchPanelProps {
 
 export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
   return (
-    <form action="">
+    <form>
       <div>
-        <input
+        {/*setParam(Object.assign({}, param, {name:evt.target.value}))*/}
+        <Input
           type="text"
           value={param.name}
-          onChange={(e) => setParam({ ...param, name: e.target.value })}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              name: evt.target.value,
+            })
+          }
         />
-        <select
+        <Select
           value={param.personId}
-          onChange={(e) => setParam({ ...param, personId: e.target.value })}
+          onChange={(value) =>
+            setParam({
+              ...param,
+              personId: value,
+            })
+          }
         >
-          <option value="">负责人</option>
+          <Select.Option value={""}>负责人</Select.Option>
           {users.map((user) => (
-            <option value={user.id} key={user.id}>
+            <Select.Option key={user.id} value={user.id}>
               {user.name}
-            </option>
+            </Select.Option>
           ))}
-        </select>
+        </Select>
       </div>
     </form>
   );
