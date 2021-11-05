d33f4e2e42cf7988c3c8ba5b89d6ed34adf8b9b7
yangdepp <yangdepp@163.com>
2021-11-06 00:15:53 +0800

    feat：useAsync

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,23 +4,26 @@ import { List } from "screens/project-list/list";
 import { cleanObject, useDebounce, useMount } from "../../utils";
 import { useHttp } from "utils/http";
 import styled from "@emotion/styled";
+import { useAsync } from "utils/use-async";
+import { Project } from "screens/project-list/list";
+import { Typography } from "antd";
 
 // 使用 JS 的同学，大部分的错误都是在 runtime(运行时) 的时候发现的
 // 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
 
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
-
   const [param, setParam] = useState({
     name: "",
     personId: "",
   });
   const debouncedParam = useDebounce(param, 200);
-  const [list, setList] = useState([]);
+
   const client = useHttp();
+  const { run, isLoading, error, data: list } = useAsync<Project[]>();
 
   useEffect(() => {
-    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
+    run(client("projects", { data: cleanObject(debouncedParam) }));
     // eslint-disable-next-line
   }, [debouncedParam]);
 
@@ -32,7 +35,10 @@ export const ProjectListScreen = () => {
     <Container>
       <h1>项目列表</h1>
       <SearchPanel users={users} param={param} setParam={setParam} />
-      <List users={users} list={list} />
+      {error ? (
+        <Typography.Text type={"danger"}>{error.message}</Typography.Text>
+      ) : null}
+      <List users={users} dataSource={list || []} loading={isLoading} />
     </Container>
   );
 };
