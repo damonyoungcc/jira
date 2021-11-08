c0275129bcd1683fa2fd8de1893a5e01a3d14286
yangdepp <yangdepp@163.com>
2021-11-08 21:57:43 +0800

    feat: 错误边界

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import { useAuth } from "context/auth-context";
 import { AuthenticatedApp } from "authenticated-app";
 import { UnauthenticatedApp } from "unauthenticated-app";
+import { ErrorBoundary } from "components/error-boundary";
+import { FullPageErrorFallback } from "components/lib";
 import "./App.css";
 
 function App() {
   const { user } = useAuth();
   return (
     <div className="App">
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
     </div>
   );
 }
