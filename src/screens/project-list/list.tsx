325a86bbfc02631216d9076a3fe3d761584ad84e
yangdepp <yangdepp@163.com>
2021-11-11 23:44:46 +0800

    feat: 看板和路由

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { User } from "screens/project-list/search-panel";
 import { Table, TableProps } from "antd";
 import dayjs from "dayjs";
+import { Link } from "react-router-dom";
 
 export interface Project {
   id: string;
@@ -26,6 +27,9 @@ export const List = ({ users, ...props }: ListProps) => {
           title: "名称",
           dataIndex: "name",
           sorter: (a, b) => a.name.localeCompare(b.name),
+          render(value, project) {
+            return <Link to={String(project.id)}>{project.name}</Link>;
+          },
         },
         {
           title: "部门",
